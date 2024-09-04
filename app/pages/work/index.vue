a9cc1d165c87f5109501ff8bb880552f5f8de0d8
seleningchan <pumbbachin@pumbba.cn>
2024-09-05 00:30:35 +0800

    Fix: id

--- app/pages/work/index.vue
+++ app/pages/work/index.vue
@@ -11,6 +11,7 @@ const { data } = await useFetch<Project[]>('/api/projects')
 console.log("print data" + data)
 for (let i = 0; i < data.length; i++) {
     if(data[i].coverImage==null){
+        console.log("fix cover image "+data[i].id)
         data[i].coverImage={ "asset": {"metadata": {"lqip": "",}, "_id": "",}}
     }
 }
