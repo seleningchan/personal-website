4babec50cbcf489442fe854d2fea56010b26626b
seleningchan <pumbbachin@pumbba.cn>
2024-09-05 21:08:47 +0800

    set no data

--- app/pages/work/index.vue
+++ app/pages/work/index.vue
@@ -8,7 +8,7 @@ useHead({
 })
 
 const {data} = await useFetch<Project[]>('/api/projects')
-console.log("v-id!")
+console.log("set no data")
 //为啥没打印，还没弄清楚
 for (let i = 0; i < data.length; i++) {
     console.log("fix cover image "+data[i].id)
@@ -34,9 +34,9 @@ const localePath = useLocalePath()
                             class="bg-cover h-full w-full rounded object-cover"
                         />
                          <my-sanity-image v-else 
-                           :src="item.id!"
+                           :src=""
                             fit="cover" height="330px"
-                            :placeholder="item.id!"
+                            :placeholder=""
                             class="bg-cover h-full w-full rounded object-cover"
                         />
                     </ui-aspect-ratio>
