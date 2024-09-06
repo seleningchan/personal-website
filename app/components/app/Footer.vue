61c355404e00008311105c39eec4c3d5178a0ab5
seleningchan <pumbbachin@pumbba.cn>
2024-09-06 22:37:30 +0800

    Change: footer

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -4,7 +4,7 @@
             <div class="flex flex-col space-y-2">
                 <div class="text-xs flex items-center space-x-1.5">
                     <span>© {{ new Date().getFullYear() }}</span>
-                    <span>Forked from</span>
+                    <span>Fork from</span>
                     <ui-button variant="ghost" class="h-7 w-7 p-0">
                         <a
                             href="https://github.com/xuzuodong/personal-website"
